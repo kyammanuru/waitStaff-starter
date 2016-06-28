ef6d031b597100751da271e52a6623aef73c2236
Yammanuru <KARUNAKAR.YAMMANURU@tetratech.com>
2016-06-28 09:31:30 -0400

    first app

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 //angularjs source file
+var app = angular.module('app', []);
+app.controller('mycontroller', function() {
+    
+	var vm = this;
+	
+	vm.subTotal = function(){
+		return vm.mealPrice;
+	};
+    
+});
